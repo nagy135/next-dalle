18281b560dfc242c9850993fe743efbb98621b1f
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-15 11:26:37 +0200

    fix: item cards

--- src/app/_components/item-card.tsx
+++ src/app/_components/item-card.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Card } from '~/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
 import type { ItemEntity } from '~/server/db/schema';
 
 const ItemCard = ({ item }: { item: ItemEntity }) => {
 	return (
 		<Card>
-			<h2>{item.name}</h2>
+			<CardHeader>
+				<CardTitle>{item.name}</CardTitle>
+				<CardDescription>{item.createdAt.toISOString()}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>img</p>
+			</CardContent>
 		</Card>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ItemCard from "./_components/item-card";
 export default async function Home() {
 	const items = await api.item.getAll();
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center text-xl">
+		<main className="flex min-h-screen flex-col items-center justify-center text-xl gap-2">
 			{items.map((item) => (
 				<ItemCard key={item.id} item={item} />
 			))}
