ca73bcf7a76a53b1854642f685cc496e6ba638c8
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-22 16:07:23 +0200

    fix: remove forgotten page

--- src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getServerAuthSession } from "~/server/auth";
-
-export default async function Login() {
-	const user = await getServerAuthSession();
-	console.log("================\n", "user: ", user, "\n================");
-	return <div>login</div>
-}
