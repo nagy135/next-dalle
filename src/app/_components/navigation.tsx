021a22867a7f134b1ba403b77e35b48b8ef31eac
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-23 19:31:50 +0200

    refactor: navigation simplify with Link

--- src/app/_components/navigation.tsx
+++ src/app/_components/navigation.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { forwardRef } from "react";
 import {
 	NavigationMenu,
 	NavigationMenuContent,
@@ -10,7 +9,6 @@ import {
 	NavigationMenuList,
 	NavigationMenuTrigger,
 } from "~/components/ui/navigation-menu"
-import { cn } from "~/lib/utils";
 import { CategoryWithChildrenEntity } from "~/server/db/schema";
 
 type NavigationProps = {
@@ -27,13 +25,20 @@ export default function Navigation({ categories }: NavigationProps) {
 						<NavigationMenuContent>
 							<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
 								{category.children?.map((child) => (
-									<ListItem
-										key={child.id}
-										title={child.name}
-										href={`/category/${child.id}`}
-									>
-										{child.description}
-									</ListItem>
+
+									<li>
+										<NavigationMenuLink asChild>
+											<Link
+												className={"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"}
+												href={`/category/${child.id}`}
+											>
+												<div className="text-sm font-medium leading-none">{child.name}</div>
+												<p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+													{child.description}
+												</p>
+											</Link>
+										</NavigationMenuLink>
+									</li>
 								))}
 							</ul>
 						</NavigationMenuContent>
@@ -43,29 +48,3 @@ export default function Navigation({ categories }: NavigationProps) {
 		</NavigationMenu>
 	)
 }
-
-const ListItem = forwardRef<
-	React.ElementRef<"a">,
-	React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-	return (
-		<li>
-			<NavigationMenuLink asChild>
-				<a
-					ref={ref}
-					className={cn(
-						"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-						className
-					)}
-					{...props}
-				>
-					<div className="text-sm font-medium leading-none">{title}</div>
-					<p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-						{children}
-					</p>
-				</a>
-			</NavigationMenuLink>
-		</li>
-	)
-})
-ListItem.displayName = "ListItem"
